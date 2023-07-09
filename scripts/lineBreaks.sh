7a44dae7372ce8e6a1fa91e9de72ee0e4390cf45
domducsutcliffe <dom.sutcliffe@crowncommercial.gov.uk>
2023-07-09 16:56:04 +0100

    hm

--- scripts/lineBreaks.sh
+++ scripts/lineBreaks.sh
@@ -3,5 +3,6 @@
 # Navigate to the required directory
 cd ..
 cd imports
-# Replace all newline characters with <br> and output to a single line
-echo $(tr '\n' '<br>' < StopReadingtheNews.md) > StopReadingtheNews.md
+
+# Replace all newline characters with <br> and insert a break at every '<<'
+echo $(tr '\n' '<br>' < StopReadingtheNews.md | sed 's/<</<br><</g') > StopReadingtheNews.md
